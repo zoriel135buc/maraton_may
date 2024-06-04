181dd93706e61ef29265aca3cd05737a58837578
zoriel135buc <zorielbuc135@gmail.com>
2024-06-04 18:04:18 +0300

    update

--- src/Bg.jsx
+++ src/Bg.jsx
@@ -5,6 +5,11 @@ function Bg() {
   return (
     <div className="bg_cont">
       <img src={close_red} className="close_red" />
+      <div className="header">
+        <div className="header_title">העלאת תמונה כדי להסיר את הרקע</div>
+        <div className="header_formats">פורמטים נתמכים : png, jpeg </div>
+        <button className="upload_img_btn ">העלאת תמונה</button>
+      </div>
     </div>
   );
 }
